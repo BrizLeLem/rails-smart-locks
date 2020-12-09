51b0e03eda7cf34c60835a60b320279e8b7feb21
Briz Le Lem <briz.lelem@gmail.com>
2020-12-09 17:26:02 +0100

    Solution to Career week Coding challenges

---
+++ db/migrate/20201209144557_create_behaviors.rb
@@ -0,0 +1,11 @@
+class CreateBehaviors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :behaviors do |t|
+      t.datetime :timestamp
+      t.string :status_change
+      t.references :lock, null: false, foreign_key: true, type: :string
+
+      t.timestamps
+    end
+  end
+end
